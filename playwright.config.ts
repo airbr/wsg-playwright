490afc10e3c98c5ae55fba54facc1f5f523f2250
airbr <morgan@morganwebdev.com>
2025-10-07 00:24:49 -0700

    Downgrade video recording

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,8 +27,8 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
-
-    video: 'on',
+    // Use Videos Sparingly but helpful as needed
+    // video: 'on',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
   },
